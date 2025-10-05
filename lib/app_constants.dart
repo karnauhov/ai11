7f6b2a03c85aa755d7c90b406b1111de637bb72e
Oleh Karnaukhov <oleh.karnaukhov@play.works>
2025-10-05 11:13:44 +0200

    Fixed main section

--- lib/app_constants.dart
+++ lib/app_constants.dart
@@ -8,6 +8,16 @@ abstract class FFAppConstants {
       'https://www.linkedin.com/in/oleg-karnauhov';
   static const String CVLink =
       'https://drive.google.com/file/d/16GGKzhAf5DtPjH1AfvGuytpPjvBYSpU0/view?usp=sharing';
+  static const int developerExperience = 21;
+  static const int webDeveloperExperience = 8;
+  static const int flutterDeveloperExperience = 1;
+  static const String languages = 'Dart, JavaScript, HTML';
+  static const String SDK = 'Flutter, Flame-engine';
+  static const String tools = 'FlutterFlow, VSCode, GitHub';
+  static const String backend = 'Firebase, Supabase, BuildShip';
+  static const String gitHubURL = 'https://github.com/karnauhov';
+  static const String freelancehuntURL =
+      'https://freelancehunt.com/freelancer/oleh_karnaukhov.html';
   static Map<String, DebugDataField> toDebugSerializableMap() => {
         'websiteURL': debugSerializeParam(
           websiteURL,
@@ -46,6 +56,95 @@ abstract class FFAppConstants {
               'https://app.flutterflow.io/project/ai11-fcaxqx?tab=appValues&appValuesTab=constant',
           searchReference:
               'reference=EhgKEAoGQ1ZMaW5rEgZ1NzJmZmJyAggDegBaBkNWTGluaw==',
+          name: 'String',
+          nullable: false,
+        ),
+        'developerExperience': debugSerializeParam(
+          developerExperience,
+          ParamType.int,
+          link:
+              'https://app.flutterflow.io/project/ai11-fcaxqx?tab=appValues&appValuesTab=constant',
+          searchReference:
+              'reference=EiUKHQoTZGV2ZWxvcGVyRXhwZXJpZW5jZRIGM2FrM3ZpcgIIAXoAWhNkZXZlbG9wZXJFeHBlcmllbmNl',
+          name: 'int',
+          nullable: false,
+        ),
+        'webDeveloperExperience': debugSerializeParam(
+          webDeveloperExperience,
+          ParamType.int,
+          link:
+              'https://app.flutterflow.io/project/ai11-fcaxqx?tab=appValues&appValuesTab=constant',
+          searchReference:
+              'reference=EigKIAoWd2ViRGV2ZWxvcGVyRXhwZXJpZW5jZRIGbGRlZTJlcgIIAXoAWhZ3ZWJEZXZlbG9wZXJFeHBlcmllbmNl',
+          name: 'int',
+          nullable: false,
+        ),
+        'flutterDeveloperExperience': debugSerializeParam(
+          flutterDeveloperExperience,
+          ParamType.int,
+          link:
+              'https://app.flutterflow.io/project/ai11-fcaxqx?tab=appValues&appValuesTab=constant',
+          searchReference:
+              'reference=EiwKJAoaZmx1dHRlckRldmVsb3BlckV4cGVyaWVuY2USBmF3ZXRhbHICCAF6AFoaZmx1dHRlckRldmVsb3BlckV4cGVyaWVuY2U=',
+          name: 'int',
+          nullable: false,
+        ),
+        'languages': debugSerializeParam(
+          languages,
+          ParamType.String,
+          link:
+              'https://app.flutterflow.io/project/ai11-fcaxqx?tab=appValues&appValuesTab=constant',
+          searchReference:
+              'reference=EhsKEwoJbGFuZ3VhZ2VzEgZoMHV6cmVyAggDegBaCWxhbmd1YWdlcw==',
+          name: 'String',
+          nullable: false,
+        ),
+        'SDK': debugSerializeParam(
+          SDK,
+          ParamType.String,
+          link:
+              'https://app.flutterflow.io/project/ai11-fcaxqx?tab=appValues&appValuesTab=constant',
+          searchReference: 'reference=EhUKDQoDU0RLEgZ0YTM3dnZyAggDegBaA1NESw==',
+          name: 'String',
+          nullable: false,
+        ),
+        'tools': debugSerializeParam(
+          tools,
+          ParamType.String,
+          link:
+              'https://app.flutterflow.io/project/ai11-fcaxqx?tab=appValues&appValuesTab=constant',
+          searchReference:
+              'reference=EhcKDwoFdG9vbHMSBmN3bmRxa3ICCAN6AFoFdG9vbHM=',
+          name: 'String',
+          nullable: false,
+        ),
+        'backend': debugSerializeParam(
+          backend,
+          ParamType.String,
+          link:
+              'https://app.flutterflow.io/project/ai11-fcaxqx?tab=appValues&appValuesTab=constant',
+          searchReference:
+              'reference=EhkKEQoHYmFja2VuZBIGeW11Z3RrcgIIA3oAWgdiYWNrZW5k',
+          name: 'String',
+          nullable: false,
+        ),
+        'gitHubURL': debugSerializeParam(
+          gitHubURL,
+          ParamType.String,
+          link:
+              'https://app.flutterflow.io/project/ai11-fcaxqx?tab=appValues&appValuesTab=constant',
+          searchReference:
+              'reference=EhsKEwoJZ2l0SHViVVJMEgZ2cjB3a2RyAggDegBaCWdpdEh1YlVSTA==',
+          name: 'String',
+          nullable: false,
+        ),
+        'freelancehuntURL': debugSerializeParam(
+          freelancehuntURL,
+          ParamType.String,
+          link:
+              'https://app.flutterflow.io/project/ai11-fcaxqx?tab=appValues&appValuesTab=constant',
+          searchReference:
+              'reference=EiIKGgoQZnJlZWxhbmNlaHVudFVSTBIGYTNiOHBjcgIIA3oAWhBmcmVlbGFuY2VodW50VVJM',
           name: 'String',
           nullable: false,
         )
